6fb4f3bc5a74ed5e5dc09ec0f4e8f0933a08a8cf
a.ito <BoxPistols@users.noreply.github.com>
2024-03-11 02:13:25 +0900

    リファクター (#1)
    * login URLパスを追加 / views.pyとurls.pyに変更を追加

--- memoapp/urls.py
+++ memoapp/urls.py
@@ -1,10 +1,25 @@
 from django.urls import path
 from .views import MemoList, MemoDetail, MemoCreate, MemoDelete, MemoUpdate
+from .views import (
+    MemoList,
+    MemoDetail,
+    MemoCreate,
+    MemoDelete,
+    MemoUpdate,
+    signupfunc,
+    loginfunc,
+    logoutfunc,
+)
+
 
 urlpatterns = [
-    path("", MemoList.as_view(), name="list"),
+    path("list/", MemoList.as_view(), name="list"),
     path("detail/<int:pk>/", MemoDetail.as_view(), name="detail"),
     path("create/", MemoCreate.as_view(), name="create"),
     path("delete/<int:pk>/", MemoDelete.as_view(), name="delete"),
     path("update/<int:pk>/", MemoUpdate.as_view(), name="update"),
+    # signupfuncとloginfuncを追加
+    path("signup/", signupfunc, name="signup"),
+    path("login/", loginfunc, name="login"),
+    path("logout/", logoutfunc, name="logout"),
 ]
